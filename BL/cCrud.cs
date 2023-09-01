98976968627a7dc2888bd9cad9a7e806e2d3757e
Furkan Kılıç <youkali40@gmail.com>
2023-09-01 23:49:13 +0300

    Version 3

--- BL/cCrud.cs
+++ BL/cCrud.cs
@@ -35,6 +35,28 @@ namespace BL
             return Tools.ConnectSet(cmd);
 
         }
+        public static bool AddShipment(Shipping Ship)
+        {
+            SqlCommand cmd = new SqlCommand("AddShipment", Tools.con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ShippingName", Ship.ShippingName);
+            cmd.Parameters.AddWithValue("@DeliveredLocation", Ship.DeliveredLocation);
+            cmd.Parameters.AddWithValue("@Distance", Ship.Distance);
+            cmd.Parameters.AddWithValue("@TotalAmount", Ship.TotalAmount);
+            cmd.Parameters.AddWithValue("@EmployeeID", Ship.EmployeeID);
+            return Tools.ConnectSet(cmd);
+        }
+        public static bool AddVehicles(Vehicles vch)
+        {
+            SqlCommand cmd = new SqlCommand("AddVehicles", Tools.con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@VehicleName", vch.VehicleName);
+            cmd.Parameters.AddWithValue("@VehicleCapacity", vch.VehicleCapacity);
+            cmd.Parameters.AddWithValue("@VehicleDriver", vch.VehicleDriver);
+            cmd.Parameters.AddWithValue("@VehicleExpense", vch.VehicleExpense);
+            cmd.Parameters.AddWithValue("@EmployeeID", vch.EmployeeID);
+            return Tools.ConnectSet(cmd);
+        }
         public static bool Update(Customers cust)
         {
             SqlCommand con = new SqlCommand("UpCustomer", Tools.con);
@@ -52,7 +74,7 @@ namespace BL
 
         public static bool Delete(Customers cust)
         {
-            SqlCommand con = new SqlCommand("DelCustomer",Tools.con);
+            SqlCommand con = new SqlCommand("DelCustomer", Tools.con);
             con.CommandType = CommandType.StoredProcedure;
             con.Parameters.AddWithValue("@CustomerNo", cust.CustomerNo);
             return Tools.ConnectSet(con);
@@ -61,5 +83,44 @@ namespace BL
 
 
         }
+        public static DataTable ListShipment()
+        {
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("ListShipment", Tools.con);
+            sqlDataAdapter.SelectCommand.CommandType = System.Data.CommandType.Text;
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            return dt;
+
+        }
+        public static DataTable ListVehicles ()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("ListVehicles", Tools.con);
+            adapter.SelectCommand.CommandType = System.Data.CommandType.Text;
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+        public static int uLogin(Employees personel)
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("Emplogin", Tools.con);
+            adp.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+            adp.SelectCommand.Parameters.AddWithValue("@username", personel.Mail);
+            adp.SelectCommand.Parameters.AddWithValue("@Password", personel.EmployeePW);
+
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            if (dt.Rows.Count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+
+
+        }
+     
     }
 }
--- Cargo-Project-Katmanlı/Form1.cs
+++ Cargo-Project-Katmanlı/Form1.cs
@@ -27,24 +27,67 @@ namespace Cargo_Project_Katmanlı
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Customers cust = new Customers();
-            cust.NameSurname = textBox1.Text;
-            cust.CustomerPW = textBox2.Text;
-            cust.Adress = textBox3.Text;
-            cust.Phone = textBox4.Text;
-            cust.Mail = textBox5.Text;
-            cust.PaymentStatus = textBox6.Text;
-            cCrud.Add(cust); // ekleme metodu procdan geliyor
-            MessageBox.Show("Ekleme İşlemi Başarılı");
-            dataGridView1.DataSource = cCrud.list(); // listeleme metodu procdan geliyor 
+            if (comboBox1.SelectedIndex == 1)
+            {
+                Customers cust = new Customers();
+                cust.NameSurname = textBox1.Text;
+                cust.CustomerPW = textBox2.Text;
+                cust.Adress = textBox3.Text;
+                cust.Phone = textBox4.Text;
+                cust.Mail = textBox5.Text;
+                cust.PaymentStatus = textBox6.Text;
+                cCrud.Add(cust); // ekleme metodu procdan geliyor
+
+                dataGridView1.DataSource = cCrud.list(); // listeleme metodu procdan geliyor 
+
+                MessageBox.Show("Müşteri  Ekleme Başarılı");
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                Shipping shp = new Shipping();
+                shp.ShippingName = textBox1.Text;
+                shp.DeliveredLocation = textBox2.Text;
+                shp.Distance = textBox3.Text;
+                shp.TotalAmount = decimal.Parse(textBox4.Text);
+                shp.EmployeeID = int.Parse(textBox5.Text);
+                cCrud.AddShipment(shp);
+                MessageBox.Show("Transfer Ekleme Başarılı");
+                dataGridView1.DataSource = cCrud.ListShipment();
+
+                //DataGridViewRow row = dataGridView1.CurrentRow;
+                //textBox1.Tag = row.Cells["Transfer No"].Value.ToString();
+                //textBox1.Text = row.Cells["Transfer Adı"].Value.ToString();
+                //textBox2.Text = row.Cells["Varış Yeri"].Value.ToString();
+                //textBox3.Text = row.Cells["Mesafe"].Value.ToString();
+                //textBox4.Text = row.Cells["Toplam Tutar"].Value.ToString();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                Vehicles vehicle = new Vehicles();
+                vehicle.VehicleName = textBox1.Text;
+                vehicle.VehicleCapacity = textBox2.Text;
+                vehicle.VehicleDriver = textBox3.Text;
+                vehicle.VehicleExpense = decimal.Parse(textBox4.Text);
+                vehicle.EmployeeID = int.Parse(textBox5.Text);
+                cCrud.AddVehicles(vehicle);
+                MessageBox.Show("Araç Ekleme Başarılı");
+                dataGridView1.DataSource = cCrud.ListVehicles();
+
+            }
+            else
+            {
+                MessageBox.Show("Bir İşlem Kategorisi Seçiniz");
+            }
+
+
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Customers cust = new Customers();
 
-           dataGridView1.DataSource = cCrud.list();
+
+            dataGridView1.DataSource = cCrud.list();
 
         }
 
@@ -66,15 +109,55 @@ namespace Cargo_Project_Katmanlı
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                DataGridViewRow column = dataGridView1.CurrentRow;
+                textBox1.Tag = column.Cells["CustomerNo"].Value.ToString();
+                textBox1.Text = column.Cells["NameSurname"].Value.ToString();
+                textBox2.Text = column.Cells["CustomerPW"].Value.ToString();
+                textBox3.Text = column.Cells["Adress"].Value.ToString();
+                textBox4.Text = column.Cells["Phone"].Value.ToString();
+                textBox5.Text = column.Cells["Mail"].Value.ToString();
+                textBox6.Text = column.Cells["PaymentStatus"].Value.ToString();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                DataGridViewRow row = dataGridView1.CurrentRow;
+                textBox1.Tag = row.Cells["Transfer No"].Value.ToString();
+                textBox1.Text = row.Cells["Transfer Adı"].Value.ToString();
+                textBox2.Text = row.Cells["Varış Yeri"].Value.ToString();
+                textBox3.Text = row.Cells["Mesafe"].Value.ToString();
+                textBox4.Text = row.Cells["Toplam Tutar"].Value.ToString();
+                textBox5.Text = row.Cells["Sorumlu No"].Value.ToString();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                DataGridViewRow row = dataGridView1.CurrentRow;
+                textBox1.Tag = row.Cells["Araç No"].Value.ToString();
+                textBox1.Text = row.Cells["Araç Adı"].Value.ToString();
+                textBox2.Text = row.Cells["Araç Kapasitesi"].Value.ToString();
+                textBox3.Text = row.Cells["Sürücü Adı"].Value.ToString();
+                textBox4.Text = row.Cells["Araç Harcaması"].Value.ToString();
+            }
+            else
+            {
+                MessageBox.Show("işlem tipi seçiniz");
 
-            DataGridViewRow column = this.dataGridView1.CurrentRow;
-            textBox1.Tag = column.Cells["CustomerNo"].Value.ToString();
-            textBox1.Text = column.Cells["NameSurname"].Value.ToString();
-            textBox2.Text = column.Cells["CustomerPW"].Value.ToString();
-            textBox3.Text = column.Cells["Adress"].Value.ToString();
-            textBox4.Text = column.Cells["Phone"].Value.ToString();
-            textBox5.Text = column.Cells["Mail"].Value.ToString();
-            textBox6.Text = column.Cells["PaymentStatus"].Value.ToString();
+            }
+
+
+
+        }
+        internal void ClearBoxes()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+
+
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -94,6 +177,75 @@ namespace Cargo_Project_Katmanlı
         private void tabPage2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 2)
+            {
+                ClearBoxes();
+                label4.Hide();
+                textBox6.Hide();
+
+
+                textBox5.Show();
+                label5.Show();
+
+                label1.Text = "Transfer Adı";
+                label2.Text = "Varış Yeri";
+                label3.Text = "Mesafe";
+                label6.Text = "Toplam Tutar";
+                label5.Text = "Sorumlu NO";
+
+
+                dataGridView1.DataSource = cCrud.ListShipment();
+
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                ClearBoxes();
+
+                label4.Show();
+                label5.Show();
+
+                textBox5.Show();
+                textBox6.Show();
+
+                label1.Text = "Adı Soyadı";
+                label2.Text = "Şifre";
+                label3.Text = "Adres";
+                label6.Text = "Telefon";
+
+                label5.Text = "Mail";
+
+                dataGridView1.DataSource = cCrud.list();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+
+                ClearBoxes();
+
+                label5.Show();
+                label4.Hide();
+
+                textBox5.Show();
+                textBox6.Hide();
+
+                label1.Text = "Araç Adı";
+                label2.Text = "Araç Kapasitesi";
+                label3.Text = "Sürücü Adı";
+                label6.Text = "Araç Harcaması";
+                label5.Text = "Sorumlu NO";
+
+
+                dataGridView1.DataSource = cCrud.ListVehicles();
+
+            }
+            else
+            {
+                ClearBoxes();
+                MessageBox.Show("Bir İşlem Kategorisi Seçiniz");
+            }
         }
     }
 }
